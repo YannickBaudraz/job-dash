f3a4042eeb1fc1c99e2ca5db77dad5bfb2bb6a9d
Yannick Clifford <yannickbaudrazdev@gmail.com>
2023-02-22 14:44:28 +0100

    Use package.json scripts for vite checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
-import vitePluginHtmlEnv from 'vite-plugin-html-env'; // https://vitejs.dev/config/
+import vitePluginHtmlEnv from 'vite-plugin-html-env';
+import packageJson from './package.json'; // https://vitejs.dev/config/
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +12,10 @@ export default defineConfig({
     checker({
       typescript: true,
       eslint: {
-        lintCommand: 'eslint --ext .ts,.tsx src',
+        lintCommand: packageJson.scripts['eslint:check'],
+      },
+      stylelint: {
+        lintCommand: packageJson.scripts['stylelint:check'],
       },
     }),
   ],
