dce778d927af90e066cad36d00571af6e14e39e7
Yannick Clifford <yannickbaudrazdev@gmail.com>
2023-02-23 02:10:00 +0100

    Add basic sidebar

--- src/view/layout/Layout.tsx
+++ src/view/layout/Layout.tsx
@@ -1,12 +1,15 @@
 import { PropsWithChildren } from 'react';
+import Sidebar from '../navigation/Sidebar';
 import { TopBar } from '../navigation/TopBar';
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <>
-      <TopBar />
-
-      <main>{children}</main>
-    </>
+    <div className="grid grid-flow-col">
+      <Sidebar />
+      <div>
+        <TopBar />
+        <main>{children}</main>
+      </div>
+    </div>
   );
 }
---
+++ src/view/navigation/Sidebar.tsx
@@ -0,0 +1,35 @@
+/**
+ * Using tailwindcss
+ * @constructor
+ */
+export default function Sidebar() {
+  return (
+    <>
+      <aside className="min-h-screen w-64">
+        <div className="flex h-20 flex-col items-center justify-center">
+          <div className="flex items-center">
+            <span className="text-2xl font-semibold">Logo</span>
+          </div>
+        </div>
+        <nav className="px-2 py-4">
+          <ul>
+            <li className="flex items-center rounded-lg py-2 px-4 hover:bg-gray-700 hover:text-white">
+              <svg
+                className="mr-3 h-6 w-6"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M3 12h18M3 6h18M3 18h18"></path>
+              </svg>
+              <span className="font-semibold">Dashboard</span>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+    </>
+  );
+}
