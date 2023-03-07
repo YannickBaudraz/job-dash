bbddbf0edb7c13fe70ffe6e63ec75566a38292ef
Yannick Clifford <yannickbaudrazdev@gmail.com>
2023-03-07 19:28:22 +0100

    Add auth navigation

--- src/common/component/Link/NoAccountLink.tsx
+++ src/common/component/Link/NoAccountLink.tsx
@@ -1,12 +1,17 @@
 import { Button } from '@material-tailwind/react';
+import { Link } from 'react-router-dom';
+import { route } from '../../../routing/routes';
 
 export default function NoAccountLink() {
   return (
-    <div className="flex items-center justify-center gap-2">
+    <Link
+      to={route('register')}
+      className="flex items-center justify-center gap-2"
+    >
       <span className="text-gray-600">Don&apos;t have an account?</span>
       <Button color="purple" variant="text" size="sm">
         Register
       </Button>
-    </div>
+    </Link>
   );
 }
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,16 +1,45 @@
 import { RouteObject } from 'react-router-dom';
+import AuthLayout from '../view/layout/AuthLayout';
 import DashboardLayout from '../view/layout/DashboardLayout';
 import AllJobs from '../view/page/AllJobs';
+import Login from '../view/page/Login';
+import Register from '../view/page/Register';
 import Stats from '../view/page/Stats';
+
+type RoutePathMap<RoutePath extends Record<string, string>> = {
+  readonly [K in keyof RoutePath]: RoutePath[K];
+};
+
+const RoutePaths = {
+  login: '/login',
+  register: '/register',
+  home: '/',
+  jobs: '/jobs',
+} as const;
+
+type RoutePath = RoutePathMap<typeof RoutePaths>;
+
+function route<T extends keyof RoutePath>(path: T): RoutePath[T] {
+  return RoutePaths[path];
+}
 
 const routes: RouteObject[] = [
   {
+    element: <AuthLayout />,
+    children: [
+      { path: route('login'), element: <Login />, index: true },
+      { path: route('register'), element: <Register /> },
+    ],
+  },
+  {
     element: <DashboardLayout />,
     children: [
-      { index: true, element: <Stats /> },
-      { path: '/all-jobs', element: <AllJobs /> },
+      { path: route('home'), element: <Stats />, index: true },
+      { path: route('jobs'), element: <AllJobs /> },
     ],
   },
 ];
+
+export { route };
 
 export default routes;
