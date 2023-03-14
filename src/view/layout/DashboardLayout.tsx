e68746eb88da463f8de6b464f18d4b5e681fb432
Yannick Clifford <yannickbaudrazdev@gmail.com>
2023-03-14 08:15:02 +0100

    Show laptop sidebar only on large screen

--- src/view/layout/DashboardLayout.tsx
+++ src/view/layout/DashboardLayout.tsx
@@ -1,13 +1,19 @@
 import { Outlet } from 'react-router-dom';
+import useMediaQuery, {
+  MediaRule,
+  MediaSize,
+} from '../../common/hook/useMediaQuery';
 import AuthGate from '../../routing/AuthGate';
-import Sidebar from '../navigation/Sidebar';
+import LaptopSidebar from '../navigation/LaptopSidebar';
 import { TopBar } from '../navigation/TopBar';
 
 export default function DashboardLayout() {
+  const isLaptop = useMediaQuery(MediaRule.Min, MediaSize.Lg);
+
   return (
     <AuthGate>
       <div className="grid grid-cols-[auto,1fr] bg-blue-gray-50/50">
-        <Sidebar />
+        {isLaptop && <LaptopSidebar />}
         <div>
           <TopBar />
           <main className="mx-auto w-[90%] pt-10">
